4fcd88811c08e600c25efd2a20d4dfa08af6b664
akinsoug <akinsoug@uncc.edu>
2015-11-11 15:10:56 +0000

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,11 +1,23 @@
-# puts "Hello, World"
+# # # puts "Hello, World"
 
-# puts "Hello, #{ARGV.first}!"
+# # # puts "Hello, #{ARGV.first}!"
 
 
+# # # name = ARGV.first || "World"
+# # # puts "Hello, #{name}!"
+
+# # # Default is "World"
+# # name = ARGV.first || "World"
+# # puts "Hello, #{name}!"
+
+# # Default is World
+# # Author: Jim Weirich
 # name = ARGV.first || "World"
+
 # puts "Hello, #{name}!"
 
-# Default is "World"
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
+
 puts "Hello, #{name}!"
