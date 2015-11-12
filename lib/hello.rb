1060d0fa5647569ef5d3e67b31ff7e6676a82343
Jim Weirich <jim (at) edgecase.com>
2015-11-12 21:08:45 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,23 +1,32 @@
-# # # puts "Hello, World"
+# # # # puts "Hello, World"
 
-# # # puts "Hello, #{ARGV.first}!"
+# # # # puts "Hello, #{ARGV.first}!"
 
 
+# # # # name = ARGV.first || "World"
+# # # # puts "Hello, #{name}!"
+
+# # # # Default is "World"
 # # # name = ARGV.first || "World"
 # # # puts "Hello, #{name}!"
 
-# # # Default is "World"
+# # # Default is World
+# # # Author: Jim Weirich
 # # name = ARGV.first || "World"
+
 # # puts "Hello, #{name}!"
 
 # # Default is World
-# # Author: Jim Weirich
+# # Author: Jim Weirich (jim@somewhere.com)
 # name = ARGV.first || "World"
 
 # puts "Hello, #{name}!"
 
+
+require 'greeter'
+
 # Default is World
-# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
